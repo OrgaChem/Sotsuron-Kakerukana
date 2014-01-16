bd6efbf49274003a2cda1125a9746bd8282903c6
OrgaChem <orga.chem.job@gmail.com>
2014-01-17 00:46:42 +0900

    Time is money

--- sotsuron.js
+++ sotsuron.js
@@ -1,20 +1,31 @@
 $(function(){
+  var remain = $('#remaining');
+  var erapsed = $('#erapsed');
+
+  function elapsedToString(diff) {
+    var day = Math.floor(diff / 86400);
+    var hour = Math.floor(diff / 3600 % 24);
+    var min = Math.floor(diff / 60 % 60);
+    var sec = Math.floor(diff % 60);
+
+    var str = [
+      day, '日',
+      hour, '時間',
+      min, '分',
+      sec, '秒'
+    ].join(' ');
+
+    return str;
+  }
 
   function update(){
     var now  = new Date().getTime();
-    var dead = new Date(deadline).getTime();
-    var diff = Math.floor((dead - now) / 1000);
-    var remain = {
-      day  : Math.floor(diff / 86400),
-      hour : Math.floor(diff / 3600 % 24),
-      min  : Math.floor(diff / 60 % 60),
-      sec  : Math.floor(diff % 60)
-    };
-    var str = remain.day  + ' 日 '
-            + remain.hour + ' 時間 '
-            + remain.min  + ' 分 '
-            + remain.sec  + ' 秒 ';
-    $('.remain').html(str);
+    var deadlineMsec = new Date(deadline).getTime();
+    var startlineMsec = new Date(startline).getTime();
+    var remainingTime = Math.floor((deadlineMsec - now) / 1000);
+    var timeIsMoney = Math.floor((now - startlineMsec) / 1000);
+    remain.text(elapsedToString(remainingTime));
+    erapsed.text(elapsedToString(timeIsMoney));
   }
 
   setInterval(update, 1000);
@@ -25,5 +36,4 @@ $(function(){
   $('.progress-bar')
     .attr('aria-valuenow', progress)
     .css('width', progress+'%');
-
 });
