f402d62d94f0b96d684f3d135e3a1be589e9004f
OrgaChem <orga.chem.job@gmail.com>
2014-01-31 07:50:34 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ var name = '(~♥~)';
 // 進捗状況(0〜100)
 var progress = 100;
 
-// 修論〆切
-var deadline = '2014/1/27 16:30';
+// 修論発表会
+var deadline = '2014/2/4 16:35';
 
 // 研究室配属日
 var startline = '2012/4/1 9:00';
